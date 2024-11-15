32809a5a4d2e45ee1069e5a585537d1587739b2e
shuvamgupta861 <shuvamkg@gmail.com>
2024-11-15 16:47:12 +0530

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-import { Card } from "antd";
-import { log } from "console";
+import { Card, Layout, Menu } from "antd";
 import { useEffect, useState } from "react";
+
+const { Header, Content } = Layout;
 
 interface Products {
   name_of_product: string;
@@ -16,7 +17,6 @@ const App = () => {
       const response = await fetch("http://localhost:3000/getProductsList");
       const data = await response.json();
       setProducts(data);
-      
     } catch (e) {
       console.log("error in fetching products list ", e);
     }
@@ -24,27 +24,80 @@ const App = () => {
 
   useEffect(() => {
     getProductsListFromBackend();
-  });
+  }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        textAlign: "center",
-        gap: "20px",
-        marginLeft: "22%",
-        marginTop: "5%",
-      }}
-    >
-      {products.map((product, index) => (
-        <Card style={{ width: 1000, border: "2px solid black", gap: "20px" }}>
-          <h1>{product.name_of_product}</h1>
-          <h3>{product.short_description}</h3>
-          <h5>{product.price}</h5>
-        </Card>
-      ))}
-    </div>
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "#001529",
+          color: "white",
+          padding: "0 20px",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          TAMAZON
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          style={{
+            backgroundColor: "#001529",
+            display: "flex",
+            gap: "15px",
+            fontSize: "18px"
+          }}
+        >
+          <Menu.Item key="1" style={{ color: "white" }}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="2" style={{ color: "white" }}>
+            Products
+          </Menu.Item>
+          <Menu.Item key="3" style={{ color: "white" }}>
+            About
+          </Menu.Item>
+          <Menu.Item key="4" style={{ color: "white" }}>
+            Contact
+          </Menu.Item>
+        </Menu>
+      </Header>
+
+      <Content
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          gap: "20px",
+          marginLeft: "22%",
+          marginTop: "5%",
+        }}
+      >
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            style={{
+              width: 1000,
+              border: "2px solid black",
+              gap: "20px",
+            }}
+          >
+            <h1>{product.name_of_product}</h1>
+            <h3>{product.short_description}</h3>
+            <h5>{product.price}</h5>
+          </Card>
+        ))}
+      </Content>
+    </Layout>
   );
 };
 
